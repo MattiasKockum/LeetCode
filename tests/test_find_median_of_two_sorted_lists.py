3ee7a5a2156aaba49acb4350c408d2c3d112f85f
Mattias <mattias@kockum.net>
2023-12-23 01:32:44 +0100

    Better run time on find_median_of_two_sorted_lists

--- tests/test_find_median_of_two_sorted_lists.py
+++ tests/test_find_median_of_two_sorted_lists.py
@@ -7,7 +7,12 @@ from leetcode.find_median_of_two_sorted_lists import findMedianSortedArrays
     ([1], [1], 1),
     ([1], [1, 2], 1),
     ([1, 2], [3, 4], 2.5),
-    ([2,2,4,4], [2,2,4,4], 3)
+    ([2,2,4,4], [2,2,4,4], 3),
+    ([1, 3], [2, 7], 2.5),
+    ([1, 3], [2], 2),
+    ([], [2, 3], 2.5),
+    ([2], [1, 3, 4, 5], 3),
+    ([2], [1, 3, 4], 2.5)
 ])
 def test_find_median_of_two_sorted_lists (nums1, nums2, expected_result):
     assert findMedianSortedArrays(nums1, nums2) == expected_result
